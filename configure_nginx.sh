f3bed3358e056509ece4f338992fcf7582efb8b1
EvgeneOskin <eoskin@crystalnix.com>
2016-07-22 11:42:47 +0600

    Add client max body to nginx config.

--- configure_nginx.sh
+++ configure_nginx.sh
@@ -1,12 +1,14 @@
 PORT=${PORT:-443}
 TARGET_PORT=${TARGET_PORT:-80}
+CLIENT_MAX_BODY_SIZE=${CLIENT_MAX_BODY_SIZE:-20M}
 
 echo "Starting Proxy: $PORT"
 echo "Target Docker Port: $TARGET_PORT"
 
 cat nginx.conf.template | \
     sed "s|{{listenPort}}|$PORT|g" | \
-    sed "s|{{targetPort}}|$TARGET_PORT|g" > /etc/nginx/nginx.conf
+    sed "s|{{targetPort}}|$TARGET_PORT|g" | \
+    sed "s|{{clientMaxBodySize}}|$CLIENT_MAX_BODY_SIZE|g" > /etc/nginx/nginx.conf
 
 # Use exec so nginx can get signals directly
 exec nginx
