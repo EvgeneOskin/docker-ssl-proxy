c7cfbccc48c04631b41460b89e51f163c39c9d4a
EvgeneOskin <eoskin@crystalnix.com>
2016-09-20 15:49:49 +0600

    Fix generating cert with ADITIONAL_NAMES_CSV.

--- add_self_signed_certs.sh
+++ add_self_signed_certs.sh
@@ -6,24 +6,24 @@ if [ ! -f "/etc/nginx/certs/key.pem" ]; then
 
 
     if test -n "${ADITIONAL_NAMES_CSV}"; then
-        cp /etc/ssl/openssl.cnf /etc/ssl/openssl.cnf.bak
-        sed 's/,/,DNS:/g' >> /etc/ssl/openssl.cnf <<EOF
+        cp /etc/ssl/openssl.cnf openssl.cnf
+        sed 's/,/,DNS:/g' >> openssl.cnf <<EOF
 [SAN]
 subjectAltName=DNS:${ADITIONAL_NAMES_CSV}
 EOF
         openssl req -new -sha256 -key key.pem -nodes \
             -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=${DOMAIN_NAME:-www.example.com}" \
-            -reqexts SAN \
-            -config /etc/ssl/openssl.cnf \
+            -reqexts SAN -config openssl.cnf \
             -out csr.pem
+        openssl x509 -req -in csr.pem \
+            -extensions SAN -extfile openssl.cnf \
+            -signkey key.pem -out cert.pem
     else
         openssl req -new -sha256 -key key.pem -nodes \
             -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=${DOMAIN_NAME:-www.example.com}" \
             -out csr.pem
+        openssl x509 -req -in csr.pem -signkey key.pem -out cert.pem
     fi
 
-    openssl x509 -req -in csr.pem -signkey key.pem -out cert.pem
-
-    mv /etc/ssl/openssl.cnf.bak /etc/ssl/openssl.cnf
     )
 fi
